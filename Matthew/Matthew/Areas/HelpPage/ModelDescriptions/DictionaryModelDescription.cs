9540a02894ab8554bab639b020eb78e2f95f8098
joadelb@gmail.com <joadelb@gmail.com>
2019-09-28 09:18:22 -0400

    Primer Commit

---
+++ Matthew/Matthew/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Matthew.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Matthew/Matthew/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Matthew.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
