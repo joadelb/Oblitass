782d2d3a2f98a19c9977064b619a68c5d96b6382
joadelb@gmail.com <joadelb@gmail.com>
2019-09-28 09:32:45 -0400

    Finalizacion del modelo

---
+++ Matthew/Matthew/Models/Fairy.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Matthew.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public enum Wishes
+    {
+        Jarana,
+        Jugar,
+        Jengibre
+
+    }
+    public class Fairy
+    {
+        [Key]
+        public int FairyID { get; set; }
+
+        [Required]
+        [Display(Description ="Nombre Completo")]
+        public string NickName { get; set; }
+        [Required]
+        public Wishes Wish { get; set; }
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+        [Display(Description ="Cumpleaños")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Birthdate { get; set; }
+
+    }
+}
